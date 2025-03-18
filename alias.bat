2bd73447ceee3a1cb7784a0ecb67cf27e2b70296
zoujilin <zoujilin@sina.com>
2025-03-18 23:00:50 +0800

    'add

---
+++ alias.bat
@@ -0,0 +1,16 @@
+:: NOT run in windows powershell
+doskey ll=dir
+doskey gss=git status -s
+doskey gll=git pull
+doskey ghh=git push
+doskey gmm=git commit -m
+doskey gaa=git add
+doskey gdf=git diff
+doskey gco=git checkout
+doskey gls=git ls-files
+date /t
+:: log some
+:: in dos, rd img    // just delete a folder named img
+:: git config --list
+:: git config --global init.defaultbranch main
+
